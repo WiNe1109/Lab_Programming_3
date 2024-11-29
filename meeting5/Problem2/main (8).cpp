0c66d3953061a89e89f92c74285a271becd1e8a7
WiNe1109 <dwiawesome8@gmail.com>
2024-11-29 18:48:54 +0700

    Add files via upload

---
+++ meeting5/Problem2/main (8).cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> // For std::reverse
+using namespace std;
+
+// Function to reverse a vector
+vector<int> reversevector(const vector<int>& input) {
+    vector<int> reversed(input);
+    reverse(reversed.begin(), reversed.end());
+    return reversed;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+  
+    if (n <= 0) {
+        cout << "The number of elements should be positive." << endl;
+        return 0;
+    }
+
+    
+    vector<int> vec(n);
+
+    cout << "Enter the elements of the vector: ";
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+    }
+
+  
+    vector<int> reversedvec = reversevector(vec);
+
+  
+    cout << "The reversed vector is: ";
+    for (int i : reversedvec) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
